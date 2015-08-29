164ecbb5b7473ac015b677545c4803f3aa20bfd2
tscheys <tim.scheys@gmail.com>
2015-08-28 21:01:02 -0700

    modified collisions algorithm

--- client/watchout.js
+++ client/watchout.js
@@ -5,6 +5,9 @@ var svgContainer = d3.select('body').append('svg')
 
 var imageWidth = 100;
 var imageHeight = 200;
+var timCollisions = 0;
+var blaineCollisions = 0;
+var scoreboard = d3.select('.collisions > span');
 
 var getRandomInBoundsIndex = function(direction) {
   var index = Math.random()*d3.select('svg').attr(direction);
@@ -21,20 +24,19 @@ var checkBounds = function (index, direction) {
   return index;
 }
 
-// var amountOfCircles = 40;
-// for(var i = 0; i < amountOfCircles; i++) {
-//   svgContainer.append('circle')
-//               .attr('cx', getRandomInBoundsIndex('width'))
-//               .attr('cy', getRandomInBoundsIndex('height'))
-//               .attr('r', 10)
-//               .attr('class', 'enemy')
-//               .style('position', 'relative')
-//               imgs.enter()
-//               .append("lib/boeing.png");
-// }
+function collide(node1X, node1Y, node2X, node2Y) {
+  // compute r for each node 
+  //  subsequently calculate nx1,nx2,... for each node 
+  var distance = Math.sqrt(Math.pow((node2Y - node1Y),2) + Math.pow((node2X - node1X),2));
+  var x = imageWidth/2;
+  var y = imageHeight/2;
 
-var amountOfCircles = 15;
-for(var i = 0; i < amountOfCircles; i++) {
+  var halfDist = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+  return distance < halfDist*2;
+}
+
+var amountOfEnemies = 1;
+for(var i = 0; i < amountOfEnemies; i++) {
   svgContainer.append('image')
               .attr('x', getRandomInBoundsIndex('width'))
               .attr('y', getRandomInBoundsIndex('height'))
@@ -43,40 +45,6 @@ for(var i = 0; i < amountOfCircles; i++) {
               .attr('width', imageWidth + 'px')
               .attr('height', imageHeight + 'px');
 }
-
-// setInterval(
-//   function(){
-//     d3.select('svg').selectAll('circle').transition()
-//                        .attr('cx', function(d){ return getRandomInBoundsIndex('width') })
-//                        .attr('cy', function(d){ return getRandomInBoundsIndex('height') });
-//   }, 1000) 
-
-// var drag = d3.behavior.drag()  
-//              .on('drag', function() { tim.attr('x', d3.event.x)
-//                                             tim.attr('y',  d3.event.y - d3.select('.scoreboard').attr('heigth')); })
-// var move = d3.behavior.drag()
-//             .on('keydown', function() {
-//                 switch(d3.event.keyCode) {
-//                     case 37: // left
-//                     tim.attr('x', tim.attr('x') + 10);
-//                     break;
-//                     case 38: // up
-//                     tim.attr('y', tim.attr('y') - 10);
-//                     break;
-
-//                     case 39: // right
-//                     tim.attr('x', tim.attr('x') - 10);
-//                     break;
-
-//                     case 40: // down
-//                     tim.attr('y', tim.attr('y') + 10);
-//                     break;
-
-//                     default: 
-//                     return; // exit this handler for other keys
-//                 }
-//                 e.preventDefault(); // prevent the default action (scroll / move caret)
-//             });
 
 var tim = svgContainer.append('image')
                              .attr('class', 'tim')
@@ -93,41 +61,52 @@ var blaine = svgContainer.append('image')
                              .attr('y', d3.select('svg').attr('height')/2)
                              .attr('xlink:href', 'lib/KillenIt.png')
                              .attr('width', imageWidth + 'px')
-                             .attr('height', imageHeight + 'px')
+                             .attr('height', imageHeight + 'px');
+
+var moveSpeed = 20;
+var getRestrictedIndex = function (hero, axis, direction) {
+  var bound = (axis === 'x') ? imageWidth : imageHeight;
+
+  if(direction === "negative") {
+    return Math.max(parseInt(d3.select(hero).attr(axis)) - moveSpeed, bound);
+  } else {
+    var dir = (axis === 'x') ? 'width' : 'height';
+    return Math.min(parseInt(d3.select(hero).attr(axis)) + moveSpeed, d3.select('svg').attr(dir) - bound);
+  }
+}
 
 d3.select('body').on('keydown', function() {
-                                  var moveSpeed = 20;
                                   switch(d3.event.keyCode) {
                                       case 37: // left
-                                        d3.select('.tim').attr('x', Math.max(parseInt(d3.select('.tim').attr('x')) - moveSpeed, imageWidth));           
+                                        d3.select('.tim').attr('x', getRestrictedIndex('.tim', 'x', 'negative'));           
                                       break;
 
                                       case 38: // up
-                                      d3.select('.tim').attr('y', Math.max(parseInt(d3.select('.tim').attr('y')) - moveSpeed, imageHeight));
+                                      d3.select('.tim').attr('y', getRestrictedIndex('.tim', 'y', 'negative'));
                                       break;
 
                                       case 39: // right
-                                      d3.select('.tim').attr('x', Math.min(parseInt(d3.select('.tim').attr('x')) + moveSpeed, d3.select('svg').attr('width') - imageWidth));
+                                      d3.select('.tim').attr('x', getRestrictedIndex('.tim', 'x', 'positive'));
                                       break;
 
                                       case 40: // down
-                                      d3.select('.tim').attr('y', Math.min(parseInt(d3.select('.tim').attr('y')) + moveSpeed, d3.select('svg').attr('height') - imageHeight));
+                                      d3.select('.tim').attr('y', getRestrictedIndex('.tim', 'y', 'positive'));
                                       break;
 
                                       case 65: // left
-                                      d3.select('.blaine').attr('x', Math.max(parseInt(d3.select('.blaine').attr('x')) - moveSpeed, imageWidth));
+                                      d3.select('.blaine').attr('x', getRestrictedIndex('.blaine', 'x', 'negative'));
                                       break;
 
                                       case 87: // up
-                                      d3.select('.blaine').attr('y', Math.max(parseInt(d3.select('.blaine').attr('y')) - moveSpeed, imageHeight));
+                                      d3.select('.blaine').attr('y', getRestrictedIndex('.blaine', 'y', 'negative'));
                                       break;
 
                                       case 68: // right
-                                      d3.select('.blaine').attr('x', Math.min(parseInt(d3.select('.blaine').attr('x')) + moveSpeed, d3.select('svg').attr('width') - imageWidth));
+                                      d3.select('.blaine').attr('x', getRestrictedIndex('.blaine', 'x', 'positive'));
                                       break;
 
                                       case 83: // down
-                                      d3.select('.blaine').attr('y', Math.min(parseInt(d3.select('.blaine').attr('y')) + moveSpeed, d3.select('svg').attr('height') - imageHeight));
+                                      d3.select('.blaine').attr('y', getRestrictedIndex('.blaine', 'y', 'positive'));
                                       break;
 
                                       default: 
@@ -138,8 +117,30 @@ d3.select('body').on('keydown', function() {
 
 setInterval(
   function(){
+  var timAlreadyCollided = false;
+  var blaineAlreadyCollided = false;
   d3.select('svg').selectAll('.enemy').transition()
                      .duration(2000)
-                     .attr('x', function(d){ return getRandomInBoundsIndex('width')  })
-                     .attr('y', function(d){ return getRandomInBoundsIndex('height')});
+                     .attr('x', function(d){ return getRandomInBoundsIndex('width')})
+                     .attr('y', function(d){ return getRandomInBoundsIndex('height')})
+                     .tween('image', function() {
+                        return function(t) {
+                          var x = d3.select(this).attr('x');
+                          var y = d3.select(this).attr('y');
+                          var blaineX = d3.select('.blaine').attr('x'); 
+                          var blaineY = d3.select('.blaine').attr('y'); 
+                          var timX = d3.select('.tim').attr('x'); 
+                          var timY = d3.select('.tim').attr('y'); 
+                          if(collide(blaineX, blaineY, x, y) && !blaineAlreadyCollided) { 
+                            blaineCollisions++;
+                            blaineAlreadyCollided = true;
+                          }
+                          if(collide(timX, timY, x, y) && !timAlreadyCollided) { 
+                            timCollisions++;
+                            timAlreadyCollided = true;
+                          }
+
+                          scoreboard.text(blaineCollisions + timCollisions);
+                        }
+                      });
 }, 2000)
